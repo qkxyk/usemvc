3046c557ef02830103060e32750c0c1ea93709c2
hx <xzc02106430@163.com>
2020-05-18 17:00:49 +0800

    addSeed

--- web/Program.cs
+++ web/Program.cs
@@ -4,23 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using web.Models;
 
-namespace web
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+namespace web {
+    public class Program {
+        public static void Main (string[] args) {
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder (args).Build ();
+            using (var scope = host.Services.CreateScope ()) {
+                var services = scope.ServiceProvider;
+                try {
+                    SeedData.Initialize (services);
+                } catch (Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>> ();
+                    logger.LogError (ex, "An Error occurred seeding the DB.");
+                }
+            }
+            host.Run ();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+            .ConfigureWebHostDefaults (webBuilder => {
+                webBuilder.UseStartup<Startup> ();
+            });
     }
 }
