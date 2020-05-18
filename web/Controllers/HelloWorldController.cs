4d9ea4b7d21b1d023c8027abd8866f7d38e0a605
hx <xzc02106430@163.com>
2020-05-18 17:33:56 +0800

    Edit

--- web/Controllers/HelloWorldController.cs
+++ web/Controllers/HelloWorldController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MvcMovie.Data;
+using web.Models;
 
 namespace web.Controllers {
     public class HelloWorldController : Controller {
@@ -34,5 +36,48 @@ namespace web.Controllers {
             return View (movie);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit (int? id) {
+            if (id == null) {
+                return NotFound ();
+            }
+            var movie = await context.Movie.FindAsync (id);
+            if (movie == null) {
+                return NotFound ();
+            }
+            return View (movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit (int id, Movie movie) {
+            if(id!=movie.Id){
+                return NotFound();
+            }
+            if(ModelState.IsValid){
+                try{
+                        context.Update(movie);
+                        await context.SaveChangesAsync();
+                }
+                catch(DbUpdateConcurrencyException){
+                    if(!MovieExists(movie.Id)){
+                        return NotFound();
+                    }
+                    else{
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        private bool MovieExists(int id)
+        {
+            var data =context.Movie.Find(id);
+            if(data==null){
+                return false;
+            }
+            return true;
+        }
     }
 }
--- web/Models/Movie.cs
+++ web/Models/Movie.cs
@@ -1,16 +1,18 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace web.Models
-{
-    public class Movie
-    {
-       public int Id { get; set; }
+namespace web.Models {
+    public class Movie {
+        public int Id { get; set; }
         public string Title { get; set; }
 
-        [DataType(DataType.Date)]
+        [Display (Name = "Release Date")]
+        [DataType (DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        public decimal Price { get; set; } 
+
+        [Column (TypeName = "decimal(18,2")]
+        public decimal Price { get; set; }
     }
 }
